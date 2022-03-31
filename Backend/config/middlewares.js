961931feae301d70820da4cdb9d11179a83e3f59
Alejandro <Alex_bcn10@hotmail.es>
2022-03-31 21:35:49 +0200

    añadida libreria bcrypt para hashear y comprobar contraseñas funcional.

--- Backend/config/middlewares.js
+++ Backend/config/middlewares.js
@@ -1,9 +1,32 @@
 //Importacion de libreria de gestion de token
 const jwt = require("jsonwebtoken");
 const Usuario = require("../usuario/usuario_model.js");
+const bcrypt = require("bcrypt");
+
+module.exports.hashear = async (contraseña) => {
+  try {
+    const hash = await bcrypt.hash(contraseña, 10);
+    return hash;
+  } catch (error) {
+    console.log(error, "Error al hashear la contraseña");
+  }
+};
+
+module.exports.comprobarHash = async (contraseñaVisible, contraseñaHasheada) => {
+  try {
+    const hashComprobado = await bcrypt.compare(
+      contraseñaVisible,
+      contraseñaHasheada
+    );
+    return hashComprobado;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 
 //Middleware para la verificación a traves de un token
-const verificacion = (comprobacionRol = null) => {
+module.exports.verificacion = (comprobacionRol = null) => {
   return async (req, res, next) => {
     try {
       const token = jwt.verify(req.headers.token, process.env.JWT_KEY);
@@ -22,4 +45,4 @@ const verificacion = (comprobacionRol = null) => {
     }
   };
 };
-module.exports = verificacion;
+
