933f7c0be187de0e8c26f43a8dc4fabfc026deda
AlexMonPe <alex_bcn10@hotmail.es>
2022-04-01 12:51:32 +0200

    añadida libreria cors para acepter fetch de otras aplicaciones

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const usRouter = require("./usuario/usuario_routes.js");
 const dotenv = require("dotenv");
 const mascotasRouter = require("./mascota/mascota_router.js");
 const citaRouter = require("./cita/cita_router.js");
+const cors = require('cors');
 
 dotenv.config();
 app.use(express.json());
+app.use(cors())
 
 app.listen(process.env.PORT, () => console.log("servidor levantado"));
 
