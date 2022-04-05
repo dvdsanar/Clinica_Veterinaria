7081f4c981ed555da7ee262fada26f92931c7acd
DESKTOP-JR2D966\Padres <dvdsanar@gmail.com>
2022-04-05 16:45:16 +0200

    Correccion ruta mascotas

--- mascota/mascota_router.js
+++ mascota/mascota_router.js
@@ -5,7 +5,7 @@ const controller = require("./mascota_controller.js");
 const verificacion = require("../config/middlewares.js");
 
 router.get("/", controller.infoMascota); //GET mascotas del usuario introducido por query params
-router.get("/allmascotas", controller.MascotasAll) // Get de todas las Mascotas
+router.get("/allmascotas", controller.mascotasAll); // Get de todas las Mascotas
 router.get("/:id", controller.mascotaPorId); // Get mascotas por iD Mascota
 router.post("/", controller.crearMascota); // Crear una nueva mascota
 router.patch("/:id", controller.modificarMascota); // Modificar mascota por id mascota
